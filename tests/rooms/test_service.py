9df17fe01b20be68ab375f886ebac15a8ec3bfb6
JimmyPage92 <mipas10@gmail.com>
2023-02-01 19:30:14 +0100

    commiting

--- tests/rooms/test_service.py
+++ tests/rooms/test_service.py
@@ -3,7 +3,7 @@ import unittest
 from src.api.rooms.schemas import RoomCreateData
 from src.api.rooms.service import create_room
 from src.interfaces.db_interface import DataObject
-
+from unittest.mock import MagicMock, patch
 
 class DataInterfaceStub:
     def create(self, room_data) -> DataObject:
@@ -32,4 +32,23 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(room_data["price"], 100)
 
 
-""""""
+"""reading room --> mockowanie pokoju/endpointa??? """
+
+# class TestReadAnyRoom(unittest.TestCase):
+#
+#     def test_read_room(self, user):
+#         data = {
+#                 "id": 1,
+#                 "size": 10,
+#                 "price": 15000,
+#                 "number": "10",
+#     }
+#         mocked_response = MagicMock()
+#         mocked_response.json.return_value = data
+#         user.get.return_value = mocked_response
+#
+#
+#         with patch("requests.get", user.get):
+#             result = test_read_room()
+#             self.assertEqual(result, data)
+
