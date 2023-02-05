b1aead492f1d28f80d8278c937a40c5c20b54056
JimmyPage92 <mipas10@gmail.com>
2023-02-05 12:36:29 +0100

    commiting docker

--- src/config.py
+++ src/config.py
@@ -3,4 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    db_file_loc: str = "sqlite:///hotel.db"
+    db_file_loc: str = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
+    DEBUG: bool = True
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+    SQLALCHEMY_DATABASE_URI: str = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
